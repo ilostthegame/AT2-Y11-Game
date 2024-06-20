cfb6d5f3a1afcc1ea08250e933d6c6c3d2825bc5
ilostthegame <168492669+ilostthegame@users.noreply.github.com>
2024-06-20 13:32:57 +1000

    Finished TitleScreen game state for now.
    Added the button output case interpreter, such that it now returns next state.

--- game_states/title_screen.py
+++ game_states/title_screen.py
@@ -3,7 +3,6 @@ from game_state import GameState
 from pygame.locals import *
 from assets import GAME_ASSETS
 from sprites.button import Button
-# TODO Make an automatic positioning system for buttons, when new game/load game added.
 
 class TitleScreen(GameState):
     """
@@ -18,8 +17,8 @@ class TitleScreen(GameState):
     Methods:
         run(self) -> str: Runs all functions associated with TitleScreen. To be called each iteration of game loop.
             Returns the next state game is to enter.
-        initialiseButtons(self): Creates start, quit buttons and adds them to button_group 
-        savedGameExist(self): Evaluates whether a saved gamefile exists. Returns True/False
+        initialiseButtons(self) -> None: Creates start, quit buttons and adds them to button_group 
+        savedGameExist(self) -> bool: Evaluates whether a saved gamefile exists. Returns True/False
     """
 
     # Attributes
@@ -43,7 +42,7 @@ class TitleScreen(GameState):
     def run(self, pygame_events, mouse_pos) -> str: 
         """
         Runs all functions associated with TitleScreen. To be called each iteration of game loop.
-            Returns the next state game is to enter.
+        Returns the next state game is to enter: in [title_screen, game_world, exit]
         """
         button_outputs = list() # List of all outputs from activated buttons
 
@@ -63,14 +62,21 @@ class TitleScreen(GameState):
 
             else:
                 continue
+            
+        # If there exists button output(s), interprets the first one in button_outputs.
+        if button_outputs:
+            foo = button_outputs[0] # TODO fix name, and make the new_game, load_game cases activate some initialisation function. Perhaps within game_world
+            match foo:
+                case 'new_game': 
+                    return 'game_world'
+                case 'load_game':
+                    return 'game_world'
+                case 'exit':
+                    return 'exit'
+                case _:
+                    raise Exception("Unknown button output")
 
-        # Interpret button outputs
-        for button_output in button_outputs:
-            self.setIsRunning(False) # stops TitleScreen loop if button is pressed 
-            self.setOutput(button_output) # Note that with multiple buttons pressed, this sets output to be the last output in button_output
-
-        
-        return self.getOutput() # Returns 'quit' if pygame is to quit, and 'start' if start button pressed.
+        return 'title_screen' # Re-enter title screen
             
                 
     def initialiseButtons(self) -> None:
@@ -83,7 +89,7 @@ class TitleScreen(GameState):
                                  'New Game',
                                  32,
                                  (200, 100, 0),
-                                 'new game',
+                                 'new_game',
                                  (600, 200))
         exit_button = Button(pygame.Surface((256, 128)), 
                                  'Exit',
@@ -99,7 +105,7 @@ class TitleScreen(GameState):
                                       'Load Game',
                                       32,
                                       (200, 200, 200),
-                                      'load game',
+                                      'load_game',
                                       (400, 200))
             button_group.add(load_game_button)
         
--- zMiscTools/test2.py
+++ zMiscTools/test2.py
@@ -1,11 +1,4 @@
-e = input()
-
-match e:
-    case '5':
-        print('5')
-    case 'e':
-        print('e')
-    case 'fff':
-        print('hello world')
-    case _:
-        print('no')
+x = list()
+x.append('a')
+if x:
+    print('a')
